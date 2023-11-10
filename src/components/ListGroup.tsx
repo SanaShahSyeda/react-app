e2bbe00cce39424d7340d50ff2018cfffc6059aa
Syeda Sana <sanashaaah4@gmail.com>
2023-11-10 11:08:34 +0500

    rendered lists.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,8 @@
 import { Fragment } from "react";
+
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     // Multiple components aren't allowed to be returned in react but must be wraped in a div/fragment.
     // By using div, an extra element is created in react, so react prefers fragments.
@@ -8,11 +11,11 @@ function ListGroup() {
     <>
       <h1>List Group</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {/* Rendering Lists dynamically */}
+        {items.map((item) => (
+          // key prop applied so react could keep record of elements
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
     // </Fragment>
