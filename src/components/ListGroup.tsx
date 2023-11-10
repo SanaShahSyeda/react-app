3e78640b895af6faa1f4b347b14e02aa0b319151
Syeda Sana <sanashaaah4@gmail.com>
2023-11-10 10:58:48 +0500

    added fragment.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
+import { Fragment } from "react";
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // Multiple components aren't allowed to be returned in react but must be wraped in a div/fragment.
+    // By using div, an extra element is created in react, so react prefers fragments.
+    // Another way to use fragments is to add <> brackets.
+    // <Fragment>
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+    // </Fragment>
   );
 }
 export default ListGroup;
